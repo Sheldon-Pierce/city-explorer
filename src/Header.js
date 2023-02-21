8829dc25d93ccfc154cde009e6b64e7963ce1740
Sheldon Pierce <Pierce55@icloud.com>
2023-02-21 09:48:33 -0800

    Fixed my navbar

--- src/Header.js
+++ src/Header.js
@@ -1,35 +1,55 @@
 import React from "react";
-import './App.css'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 class Header extends React.Component {
 
     render() {
         return (
-            <>
-                <header>
-                    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                        <div class="container-fluid">
-                            <p class="navbar-brand">City Explorer</p>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarCollapse">
-                                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                                    <li class="nav-item">
-                                        <p class="nav-link active" aria-current="page" >Home</p>
-                                    </li>
-                                </ul>
-                                <form class="d-flex" role="search">
-                                    < input onChange={this.props.getTargetInfo} type='text' placeholder='Seattle' ></input >
-                                    <button onClick={this.props.search}>Explore!</button>
-                                </form>
-                            </div>
-                        </div>
-                    </nav>
-                </header>
-            </>
-        );
-    }
+            <Navbar bg="light" expand="lg">
+              <Container fluid>
+                <Navbar.Brand href="#">City Explorer</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                  >
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="#action2">Link</Nav.Link>
+                    <NavDropdown title="Link" id="navbarScrollingDropdown">
+                      <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action4">
+                        Another action
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action5">
+                        Something else here
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#" disabled>
+                      Link
+                    </Nav.Link>
+                  </Nav>
+                  <Form className="d-flex">
+                    <Form.Control
+                        onChange={this.props.getTargetInfo}
+                      type="text"
+                      placeholder="Seattle"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                    <Button onClick={this.props.search} variant="outline-success">Explore!</Button>
+                  </Form>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          );    }
 }
 
 export default Header
